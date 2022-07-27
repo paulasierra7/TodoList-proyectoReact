ad044171564d9451de45bfe3df580f427fa64ece
paulasierra7 <65257680+paulasierra7@users.noreply.github.com>
2022-07-27 17:29:03 -0500

    1er Commit - To do list sin html

---
+++ src/components/todoApp.js
@@ -0,0 +1,56 @@
+import {useState} from 'react';
+
+export default function TodoApp() {
+        const [title, setTitle] = useState("Hola");
+        const [todos, setTodos] = useState([]);
+
+
+        function handleChange(e){
+            const value = e.target.value;
+            setTitle(value);
+        }
+
+        function handleSubmit(e){
+            e.preventDefault();
+
+            const newTodo = {
+                id: crypto.randomUUID(),
+                title: title,
+                completed: false
+            }
+
+            const temp = [...todos];
+            temp.unshift(newTodo);
+
+            setTodos(temp);
+        }
+
+    return <div className="todoContainer">
+            <form className="todoCreateForm" onSubmit={handleSubmit}>
+                <input 
+                    onChange={handleChange} 
+                    className="todoInput" 
+                    value={title} 
+                />
+                <input 
+                    onClick={handleSubmit}
+                    type="submit" 
+                    value="Create todo" 
+                    className="buttonCreate" 
+                />
+            </form>
+
+            <div className='todosContainer'>
+                {
+                    todos.map(item => (
+                        <div key={item.id}>{item.title}</div>
+                    ))}
+            </div>
+        </div>;
+}
+
+
+/* funcion de 1 linea: 
+        const [title, setTitle] = useState("Hola");
+        onClick={(e) => setTitle('Pau')}
+*/
